88553840feaaac536b7f28a31afe676196ffa9b3
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-03-07 18:18:46 -0300

    amend

--- src/main/java/ar/unrn/encap/mal/UsuariosManager.java
+++ src/main/java/ar/unrn/encap/mal/UsuariosManager.java
@@ -4,12 +4,15 @@ public class UsuariosManager {
 
     public void cambiarClave(Usuario u, String claveActual,
                              String nuevaClave, String nuevaClaveRepetida) {
+
         if (!u.getClave().equals(claveActual)) {
             throw new RuntimeException("No coincide con la actual...");
         }
+
         if (!nuevaClave.equals(nuevaClaveRepetida)) {
             throw new RuntimeException("No coinciden...");
         }
+        
         u.setClave(nuevaClave);
     }
 }

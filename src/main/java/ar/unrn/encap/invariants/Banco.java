892cd66c6e93556a5b1a807f7d4bcb7c265f4847
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-03-13 17:04:30 -0300

    fix comments

--- src/main/java/ar/unrn/encap/invariants/Banco.java
+++ src/main/java/ar/unrn/encap/invariants/Banco.java
@@ -6,16 +6,17 @@ public class Banco {
     public void retirarDinero(CuentaBancaria cuenta, float saldo) {
         cuenta.retirar(saldo); // Ok, y deberia ser a unica posibilidad
 
-        //dejar getters y setters me da la posibilidad de comer estos errores
+        //dejar getters y setters me da la posibilidad de cometer estos errores
         //pudiendo no cumplir con la invariante: el saldo nunca puede ser negativo
         var saldoActual = cuenta.getSaldo();
         cuenta.setSaldo(saldoActual - saldo);
     }
 
     public void transferir(CuentaBancaria cuentaOrigen, CuentaBancaria cuentaDestino, float saldo) {
-        //Por otro lado, es proble que empiece a duplicar código en Banco, por ejemplo:
+        //Por otro lado, es probable que empiece a duplicar código en Banco, por ejemplo:
         //var saldoActual = cuenta.getSaldo(); cuenta.setSaldo(saldoActual - saldo);
         //Aca es donde comienzo a generar método privados dentro de Banco para sacar codigo duplicado
         //y la solución no viene por ahi, sino por encontrar los objetos del modelo apropiados
+        //y mover lógica de negocios allí.
     }
 }

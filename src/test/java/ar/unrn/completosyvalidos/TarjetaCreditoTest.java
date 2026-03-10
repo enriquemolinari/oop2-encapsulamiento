86f99f3b8533db0f420c23a146d54e736e80834e
Enrique Molinari <enrique.molinari@gmail.com>
2026-03-10 20:53:03 -0300

    fix ejemplo

--- src/test/java/ar/unrn/completosyvalidos/TarjetaCreditoTest.java
+++ src/test/java/ar/unrn/completosyvalidos/TarjetaCreditoTest.java
@@ -2,7 +2,11 @@ package ar.unrn.completosyvalidos;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Year;
 import java.time.YearMonth;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TarjetaCreditoTest {
 
@@ -32,7 +36,21 @@ public class TarjetaCreditoTest {
     @Test
     public void test04() {
         //como testeo estaVencida() == true y estaVencida() == false?
+        var t = new TarjetaCredito3("1234567890123456",
+                "Juan Perez",
+                YearMonth.of(2024, 12),
+                "123");
+        assertTrue(t.estaVencida());
+    }
 
+    @Test
+    public void test05() {
+        //como testeo estaVencida() == true y estaVencida() == false?
+        var t = new TarjetaCredito3("1234567890123456",
+                "Juan Perez",
+                YearMonth.of(Year.now().plusYears(1).getValue(), 11),
+                "123");
+        assertFalse(t.estaVencida());
     }
 
 }
